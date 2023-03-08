e58cffd378505f478e76549156872d17ea9c733a
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-03-08 15:31:53 +0100

    tokenHelper functions

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,63 @@
 package helpers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/KrzysztofSieczkiewicz/go-authentication-service-test/database"
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+// properly should be stored in .env
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(
+	email string,
+	firstName string,
+	lastName string,
+	userType string,
+	userId string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		User_type:  userType,
+		Uid:        userId,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+}
