d2c5356c6529cf35560ad1e4a276220d6fab78b7
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-03-08 15:44:33 +0100

    Login and verifyPassword functions

--- controllers/userController.go
+++ controllers/userController.go
@@ -9,6 +9,7 @@ import (
 
 	helper "github.com/KrzysztofSieczkiewicz/go-authentication-service-test/helpers"
 	"github.com/KrzysztofSieczkiewicz/go-authentication-service-test/models"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/KrzysztofSieczkiewicz/go-authentication-service-test/database"
 	"github.com/gin-gonic/gin"
@@ -25,8 +26,17 @@ func HashPassword() {
 
 }
 
-func VerifyPassword() {
+func verifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
 
+	if err != nil {
+		msg = fmt.Sprintf("Email or password is incorrect")
+		check = false
+	}
+
+	return check, msg
 }
 
 func SignUp() gin.HandlerFunc {
@@ -96,7 +106,30 @@ func SignUp() gin.HandlerFunc {
 	}
 }
 
-func LogIn() {
+func LogIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		err := c.BindJSON(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email or password is incorrect"})
+			return
+		}
+
+		isPasswordValid, msg := verifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+
+	}
 
 }
 
