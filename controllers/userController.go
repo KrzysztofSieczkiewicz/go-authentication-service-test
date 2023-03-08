57130ca6857ce634d50fd027b9cc176d9b47a3c6
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-03-08 14:32:54 +0100

    getUser method

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	helper "github.com/KrzysztofSieczkiewicz/go-authentication-service-test/helpers"
+	"github.com/KrzysztofSieczkiewicz/go-authentication-service-test/models"
 
 	"github.com/KrzysztofSieczkiewicz/go-authentication-service-test/database"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -39,8 +43,18 @@ func GetUser() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-	}
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		var user models.User
+		userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	}
 }
 
 func GetUsers() {
